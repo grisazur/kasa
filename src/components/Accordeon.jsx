908c3640eef017c00c93a92af61ebc85e7451404
Nelly Mercey <nmercey@grisazur.com>
2023-01-06 15:59:14 +0100

    modification code accordeon pour rajouter la condition contentArray de la page Logement

--- src/components/Accordeon.jsx
+++ src/components/Accordeon.jsx
@@ -1,25 +1,49 @@
 import React from 'react'
 import { useState } from 'react'
-import ImgDown from '../assets/images/arrow.svg'
+import ImgDown from '../assets/images/arrow-down.svg'
+import ImgTop from '../assets/images/arrow-top.svg'
 
 
+{/* on doit renvoyer le titre, et 2 types de contenu : du texte simple (page about) et du texte en tableau (page logement) */}
 const Accordeon = ({ title, content, contentArray }) => {
-    const [active, setActive] = useState(false); // on initie l'accordéon en le mettant en position fermée par défaut 
+    {/* on initie l'accordéon en le mettant en position fermée par défaut */}
+    const [isActive, setIsActive] = useState(false);  
     
-    const handleToggle = () => {
-        setActive(!active)
-    } // on active le toggle si la classe est différente de false (fermée), donc true
-    
-    return (
-        // on met une condition : si la condition vaut true, l’élément juste après && sera affiché (la classe active)
-        <div className={`accordeon ${active && 'active'}`}> 
-            <div className='accordeon__title' onClick={handleToggle}>
-                <p>{title}</p> 
-               <img className='accordeon__arrow' src={ImgDown} alt='flêche bas'></img> 
+    {/* si la condition setIsActive est fausse = accordéon affiche content et flêche vers le haut, si setIsActive est true = accordéon fermé */}
+    return isActive ? (
+        <div className='accordeon__container'>
+            <div className='accordeon__header'>
+                <p className='accordeon__header__title' key='id'>{title}</p> 
+                <img className='accordeon__arrow' 
+                    src={ImgTop} 
+                    alt='flêche haut' 
+                    onClick={() => setIsActive(false)}>
+                </img>
             </div>
-            <div className='accordeon__content'>{content}</div>
+            {contentArray ? (
+                <div className='accordeon__content' key='id'>
+                    {contentArray.map((content, id) => (
+                        <p className="accordeon__content__array" key={id}>
+                            {content}
+                        </p>
+                    ))}
+                </div>
+                ) : (
+                    <p className='accordeon__content__txt'>{content}</p>
+                )}
         </div>
-    );
+        ) : (
+        <div className='accordeon__container'> 
+            <div className='accordeon__header'>
+                <p className='accordeon__header__title' key='id'>{title}</p> 
+                <img className='accordeon__arrow' 
+                    src={ImgDown} 
+                    alt='flêche bas' 
+                    onClick={() => setIsActive(true)}>
+                </img>
+            </div>
+        </div>
+        )
 };
 
 export default Accordeon
