af56bf832c59b77456c9c61cdc5a523b0fa70532
Nelly Mercey <nmercey@grisazur.com>
2023-01-14 20:04:25 +0100

    commentaires

--- src/components/Accordeon.jsx
+++ src/components/Accordeon.jsx
@@ -6,25 +6,29 @@ import ImgTop from '../assets/images/arrow-top.svg'
 
 {/* on doit renvoyer le titre, et 2 types de contenu : du texte simple (page about) et du texte en tableau (page logement) */}
 const Accordeon = ({ title, content, contentArray }) => {
+    
     {/* on initie l'accordéon en le mettant en position fermée par défaut */}
     const [isActive, setIsActive] = useState(false);  
     
-    {/* si la condition setIsActive est fausse = accordéon affiche content et flêche vers le haut, si setIsActive est true = accordéon fermé */}
+    {/* si la condition isActive est true = accordéon affiche content et flêche vers le haut, si isActive est false = accordéon fermé (puisque initialisé sur false) */}
     return isActive ? (
+
         <div className='accordeon__container'>
+
             <div className='accordeon__header'>
                 <p className='accordeon__header__title' key='id'>{title}</p> 
                 <img className='accordeon__header__arrow' src={ImgTop} alt='flêche haut' onClick={() => setIsActive(false)} />
             </div>
+
             {contentArray ? (
-                <div className='accordeon__content' key='id'>
-                    {contentArray.map((content, id) => (
-                        <p className="accordeon__content__array" key={id}> {content} </p>
-                    ))}
-                </div>
-                ) : (
-                    <p className='accordeon__content__txt'> {content} </p>
-                )}
+            <div className='accordeon__content' key='id'>
+                {contentArray.map((content, id) => (
+                <p className="accordeon__content__array" key={id}> {content} </p>
+                ))}
+            </div>
+            ) : (
+            <p className='accordeon__content__txt'> {content} </p>
+            )}
         </div>
         ) : (
         <div className='accordeon__container'> 
@@ -34,6 +38,6 @@ const Accordeon = ({ title, content, contentArray }) => {
             </div>
         </div>
         )
-};
+}
 
 export default Accordeon
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,19 +5,23 @@ import Accordeon from '../components/Accordeon'
 
 const About = () => {
   return (
+
     <div className='about'>
+
       <div className='bannier'>
           <section className='bannier__section'>
             <img className='bannier__img' src={Img} alt='photo paysage'/>
             <div className='bannier__img-overlay'></div>
           </section>
       </div>
+
       <div className='about__accordeon'>
         <Accordeon title='Fiabilité' content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."/>
         <Accordeon title='Respect' content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plataforme."/>
         <Accordeon title='Service' content= "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."/>
         <Accordeon title='Sécurité' content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
       </div>
+      
     </div>
   )
 }
